53f71a3350832846723d976a71191695292e8e56
reingenieria-estrategias <155797423+reingenieria-estrategias@users.noreply.github.com>
2024-01-12 12:15:28 -0600

    Add files via upload

---
+++ Ubicacion_Resultado_Desalojo.js
@@ -0,0 +1,13 @@
+getLocation('111345422');
+function getLocation(controlId) {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position){
+            loader.getDOMAbstractionLayer().setControlValueById(controlId,
+                position.coords.latitude +
+                "," + position.coords.longitude
+            );
+        });
+    } else {
+        console.error("Geolocation is not supported by this browser.");
+    }
+}
