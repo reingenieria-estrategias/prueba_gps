7d7b459bb94d92f06303beb5e608c8ff8d828f89
reingenieria-estrategias <155797423+reingenieria-estrategias@users.noreply.github.com>
2026-04-17 17:52:11 -0600

    Update Ubicacion_Modulo_Pre_Altas_2025.js
    Agrego cálculo de impuestos

--- Ubicacion_Modulo_Pre_Altas_2025.js
+++ Ubicacion_Modulo_Pre_Altas_2025.js
@@ -1,13 +1,31 @@
+// ===== GEOLOCALIZACION =====
 getLocation('119652394');
+
 function getLocation(controlId) {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position){
             loader.getDOMAbstractionLayer().setControlValueById(controlId,
-                position.coords.latitude +
-                "," + position.coords.longitude
+                position.coords.latitude + "," + position.coords.longitude
             );
         });
     } else {
         console.error("Geolocation is not supported by this browser.");
     }
 }
+
+
+// ===== CALCULO DE IMPUESTOS =====
+form.on('change', function(field) {
+
+  var valor = parseFloat(form.getFieldValue('field_119667588')) || 0;
+  var resultado;
+
+  if (valor <= 641000) {
+    resultado = valor * 0.015;
+  } else {
+    resultado = valor * 0.03;
+  }
+
+  form.setFieldValue('field_121006750', resultado.toFixed(2));
+
+});
