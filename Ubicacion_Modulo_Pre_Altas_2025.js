65b194ac086287358a7c022f86ae088f6e8b1b9f
reingenieria-estrategias <155797423+reingenieria-estrategias@users.noreply.github.com>
2026-04-19 00:09:02 -0600

    Update Ubicacion_Modulo_Pre_Altas_2025.js
    actualizacion

--- Ubicacion_Modulo_Pre_Altas_2025.js
+++ Ubicacion_Modulo_Pre_Altas_2025.js
@@ -1,20 +1,15 @@
 setInterval(function() {
 
-  var api = loader.engine.document;
+  var api = loader.getDOMAbstractionLayer();
 
-  function limpiar(id) {
-    try {
-      var v = api.getElementById(id).getProperty('value.value');
-      return parseFloat((v || "0").toString().replace(/,/g, '')) || 0;
-    } catch(e) {
-      return 0;
-    }
+  function limpiar(valor) {
+    return parseFloat((valor || "0").toString().replace(/,/g, '')) || 0;
   }
 
-  var lote = limpiar('119659971');
-  var mts = limpiar('119667582');
-  var precioMt2 = limpiar('119667584');
-  var costo = limpiar('119791015');
+  var lote = limpiar(api.getControlValueById('119659971'));
+  var mts = limpiar(api.getControlValueById('119667582'));
+  var precioMt2 = limpiar(api.getControlValueById('119667584'));
+  var costo = limpiar(api.getControlValueById('119791015'));
 
   var valor = lote + (mts * precioMt2) + costo;
 
@@ -24,11 +19,12 @@ setInterval(function() {
     ? valor * 0.015
     : valor * 0.03;
 
+  // 🔥 FORMATO AQUÍ
   var formateado = resultado.toLocaleString('en-US', {
     minimumFractionDigits: 2,
     maximumFractionDigits: 2
   });
 
-  api.getElementById('121009493').setValue({ value: formateado });
+  api.setControlValueById('121009493', formateado);
 
 }, 1000);
