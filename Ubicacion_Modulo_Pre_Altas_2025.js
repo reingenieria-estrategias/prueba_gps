d578d21b4dc714d93ce1b9ed839cbd3350396481
reingenieria-estrategias <155797423+reingenieria-estrategias@users.noreply.github.com>
2026-04-26 23:59:23 -0600

    Update Ubicacion_Modulo_Pre_Altas_2025.js
    ACTUALIACION

--- Ubicacion_Modulo_Pre_Altas_2025.js
+++ Ubicacion_Modulo_Pre_Altas_2025.js
@@ -1,14 +1,13 @@
-// ===== GEOLOCALIZACION =====
 getLocation('119652394');
-
 function getLocation(controlId) {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position){
-            loader.getDOMAbstractionLayer().setControlValueById(
-                controlId,
-                position.coords.latitude + "," + position.coords.longitude
+            loader.getDOMAbstractionLayer().setControlValueById(controlId,
+                position.coords.latitude +
+                "," + position.coords.longitude
             );
         });
+    } else {
+        console.error("Geolocation is not supported by this browser.");
     }
 }
-// =====
