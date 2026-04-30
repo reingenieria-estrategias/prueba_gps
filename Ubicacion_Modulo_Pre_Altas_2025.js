f437b3540cc32fe7c3c37584a791aec00b3145d4
reingenieria-estrategias <155797423+reingenieria-estrategias@users.noreply.github.com>
2026-04-30 16:39:29 -0600

    Update Ubicacion_Modulo_Pre_Altas_2025.js
    Actualizacion

--- Ubicacion_Modulo_Pre_Altas_2025.js
+++ Ubicacion_Modulo_Pre_Altas_2025.js
@@ -13,17 +13,34 @@ function getLocation(controlId) {
 }
 
 
-// ===== CALCULO DE IMPUESTOS =====
+// ===== CALCULO DE IMPUESTOS + DIFERENCIA =====
 setInterval(function() {
 
   var api = loader.getDOMAbstractionLayer();
 
+  // ===== VALOR TOTAL (campo fórmula) =====
   var valor = parseFloat(api.getControlValueById('121107880')) || 0;
 
+  // ===== IMPUESTO =====
   var resultado = (valor <= 641000)
     ? valor * 0.015
     : valor * 0.03;
 
   api.setControlValueById('121103995', resultado.toFixed(2));
+
+
+  // ===== DIFERENCIA =====
+  var avaluo = parseFloat(api.getControlValueById('121104013')) || 0;
+  var capacidad = parseFloat(api.getControlValueById('121110681')) || 0;
+  var operacion = valor;
+
+  // tomar el menor entre avalúo y capacidad
+  var base = Math.min(avaluo, capacidad);
+
+  // calcular diferencia
+  var diferencia = operacion - base;
+
+  // guardar resultado
+  api.setControlValueById('121104026', diferencia);
 
 }, 1000);
